ec680ac10d3111fe3947b533e7d4ca2f3cb647d2
Wonjaegang <robinsam@naver.com>
2021-07-27 17:56:32 +0900

    Add GUI for particles
    - Particle 들의 이동을 visualize 하는 것이 좋겠다고 판단하여 GUI_Practice 파일에서 연습한 코드를 토대로 particle 들의 이동을 격자 스크린 위에 나타내었다.

--- LinearRegression_PSO.py
+++ LinearRegression_PSO.py
@@ -9,6 +9,8 @@
 
 
 import random
+import pygame
+pygame.init()
 
 
 # 데이터 세트
@@ -17,30 +19,66 @@ dataSet_output = [18, -82, 28, -42, -62, 8, -52, -2, -12, 48, 38, -72, 58, -22, 
 
 newDataSet = [-11, 4, -9, 2, -10, -2, 0, -3, 1, -6, -7, -5, -8, -1]
 
+# GUI 관련 변수
+SCREEN_SIZE = 1000
+screen = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
+clock = pygame.time.Clock()
 
-class Particle:
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+
+CELL_SIZE = 100
+COLUMN_COUNT = 10
+ROW_COUNT = 10
+particleSize = 5
+
+
+# GUI: 격자 및 텍스트 출력 함수
+def printGrid():
+    for column_index in range(COLUMN_COUNT):
+        for row_index in range(ROW_COUNT):
+            gridRect = (CELL_SIZE * column_index, CELL_SIZE * row_index, CELL_SIZE, CELL_SIZE)
+            pygame.draw.rect(screen, WHITE, gridRect)
+            pygame.draw.rect(screen, BLACK, gridRect, 1)
+
+    font = pygame.font.SysFont('arial', 20, True, False)
+    gains = font.render("W = %.3f     C1 = %.3f     C2 = %.3f" % (Particle.w, Particle.c1, Particle.c2), True, BLACK)
+    globalBest = font.render("Global lowest loss function: %.20f" % Particle.globalBestCost, True, BLACK)
+    screen.blit(gains, (500, 20))
+    screen.blit(globalBest, (500, 45))
+
+
+class Particle(pygame.Rect):
     globalBest = [0, 0]
     globalBestCost = 0
     # c1, c2는 학습요인의 가중치이며, w는 방향벡터의 관성가중치
     c1 = 1
     c2 = 1
-    w = 0.5
+    w = 0.4
 
     # 임의의 값으로 각 particle 의 위치벡터/방향벡터 초기화
     def __init__(self):
-        self.location = [random.random() * 100, random.random() * 100]
-        self.direction = [random.random() * 100, random.random() * 100]
+        self.location = [random.uniform(-1, 1) * 500, random.uniform(-1, 1) * 500]
+        self.direction = [random.uniform(-1, 1) * 500, random.uniform(-1, 1) * 500]
         self.personalBest = self.location
         self.personalBestCost = self.calculateCost()
 
+        # GUI: 스크린의 중앙에 원점이 오도록 위치 값 초기화
+        super().__init__(list(map(lambda x: x + SCREEN_SIZE / 2, self.location)), [particleSize, particleSize])
+
     # Particle 의 이번 위치벡터/방향벡터 계산
-    def move(self):
+    def moveParticle(self):
         self.direction = list(map(lambda x:
                               self.w * x[1]
                               + Particle.c1 * random.random() * (self.personalBest[x[0]] - self.location[x[0]])
                               + Particle.c2 * random.random() * (Particle.globalBest[x[0]] - self.location[x[0]])
                               , enumerate(self.direction)))
         self.location = list(map(lambda x, y: x + y, self.location, self.direction))
+
+        # GUI: 위치값 업데이트
+        self.left = self.location[0] + SCREEN_SIZE / 2
+        self.top = self.location[1] + SCREEN_SIZE / 2
 
     # 현재의 손실함수 값과 자신의 최저손실함수 값, 전역최저손실함수 값을 비교하여 업데이트
     def updateBest(self):
@@ -81,12 +119,17 @@ if __name__ == "__main__":
     particleList[0].initializeGlobalBest()
 
     # 주어진 iteration 동안 particle 들이 이동 및 최저손실함수 업데이트
-    iteration = 500
+    # GUI: iteration 마다 particle 위치 업데이트 후 출력
+    iteration = 1000
     for simulation in range(iteration):
+        printGrid()
         for particle in particleList:
-            particle.move()
+            particle.moveParticle()
             particle.updateBest()
+            pygame.draw.rect(screen, RED, particle)
         print("Iteration #%d - Global lowest cost: %.20f" % (simulation, Particle.globalBestCost))
+        pygame.display.update()
+        pygame.time.wait(50)
 
     # Particle 들의 평균 최종위치 계산
     averageLocation = [0, 0]
